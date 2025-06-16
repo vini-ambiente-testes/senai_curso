fc25cd75cabb18941e64b369871e6e4870b03d6c
vini-ambiente-testes <vini.ambiente.testes@gmail.com>
2025-06-16 22:49:16 +0000

    Adição da pasta lista_12_06

---
+++ lista_12_06/calculadora_basic.java
@@ -0,0 +1,42 @@
+/*
+Exercício 1: Calculadora Básica
+Crie um programa que receba dois números do usuário e permita realizar as 4 operações básicas (+, -, *, /) conforme a escolha do usuário.
+*/
+import java.util.Scanner;
+public class calculadora_basic{
+  public static void numeros(double primeiro, double segundo, String simbolo){
+    double operacao=0;
+    switch (simbolo) {
+      case "+":
+        operacao=primeiro+segundo;
+        System.out.println("A soma dos números é: "+operacao);
+      break;
+      case "-":
+        operacao=primeiro-segundo;
+        System.out.println("A Diferença dos números é: "+operacao);
+      break;
+      case "*":
+        operacao=primeiro*segundo;
+        System.out.println("A Multiplicação dos números é: "+operacao);
+      break;
+      case "/":
+        operacao=primeiro/segundo;
+        System.out.println("A Subtração dos números é: "+operacao);
+      break;
+      default:
+        System.out.println("Você digitou errado algum desses valores");
+      };
+  };  
+  public static void main(String[] args) {
+    Scanner leitura = new Scanner(System.in);
+    System.out.println("Calculadora Básica");
+    System.out.println("Digite o 1° número: ");
+    double primeiro=leitura.nextDouble();
+    System.out.println("Digite o 2° número: ");
+    double segundo=leitura.nextDouble();
+    System.out.println("Digite a operação que quer fazer com esses números (+, -, *, /)? ");
+    String simbolo=leitura.next();
+    leitura.close();
+    numeros(primeiro,segundo,simbolo);  
+  }
+}
--- novo/CalculadoraMediaPonderada.java
+++ novo/CalculadoraMediaPonderada.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+c
 
 public class CalculadoraMediaPonderada {
 
