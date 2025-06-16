5b74fa4fddf87577c93cb9435751209cd244cae6
vini-ambiente-testes <vini.ambiente.testes@gmail.com>
2025-06-16 23:45:44 +0000

    Adição dos exercícios: calculadora_basic e mostrar_tabuada

---
+++ lista_12_06/mostrar_tabuada.java
@@ -0,0 +1,24 @@
+/*
+Exercício 4: Tabuada
+Peça ao usuário um número e mostre a tabuada de 1 a 10 desse número.
+*/
+import java.util.Scanner;
+public class mostrar_tabuada{
+  public static void numeros(int numero){
+    int contador=0;
+    int operacao=0;
+    while(contador<=9 && contador>=0){
+      contador=contador+1;
+      operacao=numero*contador;
+      System.out.println(numero+"x"+contador+" = "+operacao);
+    }
+  }
+  public static void main(String[] args) {
+    Scanner leitura = new Scanner(System.in);
+    System.out.println("Tabuada Básica");
+    System.out.println("Digite o número para ver tabuada: ");
+    int numero=leitura.nextInt();
+    leitura.close();
+    numeros(numero);  
+  }
+}
