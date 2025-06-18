7e342e64aa6894c293d283be14e79cd1ec61925f
vini-ambiente-testes <vini.ambiente.testes@gmail.com>
2025-06-18 23:36:56 +0000

    erros no contador_vogais.java

--- lista_12_06/contador_vogais.java
+++ lista_12_06/contador_vogais.java
@@ -4,7 +4,10 @@ Peça ao usuário uma palavra e conte quantas vogais (a, e, i, o, u) ela possui.
 */
 import java.util.Scanner;
 public class contador_vogais{
-  public static void captura_vogais(char indice[]){
+  public static void resposta (String resposta){
+     System.out.println("O número de vogais é: "+resposta);
+  }
+  public static void captura_vogais(char indice){
     String indice_str=Character.toString(indice);
     int resultado=0;
     switch (indice_str) {
@@ -22,14 +25,16 @@ public class contador_vogais{
       break;
       case "u":
         resultado=resultado+1;
-      break;
-    };
-  }
-  public static void criador_array(String palavra){
-    char[] lista_palavra = palavra.toCharArray();
-    String  = new String(lista_palavra);
-    captura_vogais(char lista_palavra[]);
-  }
+      break;      
+    }
+    resposta(Integer.toString(resultado));
+ }
+  public static void  criador_array(String palavra){
+    char[] lista_palavra=palavra.toCharArray(); 
+    for(char indice: lista_palavra){
+      captura_vogais(indice);
+    }
+  }  
   public static void main(String[] args) {
     Scanner leitura = new Scanner(System.in);
     System.out.println("Contador de vogais");
