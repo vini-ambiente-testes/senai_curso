f7cb2da31edd44b7161bfe343bf0a2d9b00b4b67
Vinícius Bruno <vinimerces2016@gmail.com>
2025-06-22 09:00:03 -0300

    primeiras funções do jogo de adivinhação concluídas em parte

--- lista_12_06/jogo_adivinhacao.java
+++ lista_12_06/jogo_adivinhacao.java
@@ -3,12 +3,17 @@ Exercício 9: Jogo de Adivinhação
 Crie um jogo onde o computador gera um número aleatório entre 1 e 100 e o usuário tenta
 adivinhar.
 */
-//Scanner leitura = new Scanner(System.in);
 import java.util.Scanner;
 import java.util.Random;
 public class jogo_adivinhacao{
-  public static void resposta_usuario(int randomNumber){
+  public static void processamento(int palpite){
 
+  }
+  public static void resposta_usuario(int randomNumber){
+    Scanner leitura = new Scanner(System.in);
+    System.out.println("Digite o seu palpite");
+    int palpite=leitura.nextInt();
+    processamento(palpite);
   }
   public static void gerador(){
     Random random = new Random();
