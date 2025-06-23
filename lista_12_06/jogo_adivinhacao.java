fcc302f7bf10c0c554a22d41f574b982fd7fabf7
Vinícius Bruno <vinimerces2016@gmail.com>
2025-06-23 09:05:24 -0300

    incompleto

--- lista_12_06/jogo_adivinhacao.java
+++ lista_12_06/jogo_adivinhacao.java
@@ -6,19 +6,20 @@ adivinhar.
 import java.util.Scanner;
 import java.util.Random;
 public class jogo_adivinhacao{
-  public static void processamento(int palpite){
-
+  public static void processamento(int [] lista_binaria, int randomNumber){
+    System.out.println(randomNumber);
+    for(int indice: lista_binaria){
+      System.out.println(indice);
+    }
   }
-  public static void resposta_usuario(int randomNumber){
-    Scanner leitura = new Scanner(System.in);
-    System.out.println("Digite o seu palpite");
-    int palpite=leitura.nextInt();
-    processamento(palpite);
+  public static void valores(int randomNumber){
+    int[] lista_binaria=new int[99];
+    processamento(lista_binaria,randomNumber);
   }
   public static void gerador(){
     Random random = new Random();
     int randomNumber = random.nextInt(100);
-    resposta_usuario(randomNumber);
+    valores(randomNumber);
   }
   public static void main(String[] args) {
     System.out.println("Jogo de adivinhação");
