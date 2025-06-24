7830da7da3e7a74633a48971f6e3f8743ae66e98
vini-ambiente-testes <vini.ambiente.testes@gmail.com>
2025-06-24 23:00:12 +0000

    vários erros lógica no jogo_adivinhacao.java

--- lista_12_06/jogo_adivinhacao.java
+++ lista_12_06/jogo_adivinhacao.java
@@ -1,16 +1,29 @@
-/*
+      /*
 Exercício 9: Jogo de Adivinhação
 Crie um jogo onde o computador gera um número aleatório entre 1 e 100 e o usuário tenta
 adivinhar.
 */
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 public class jogo_adivinhacao{
-  public static  int processamento(int randomNumber){
-    int meio=randomNumber/2;
-    int fim=randomNumber;
-    int inicio=0;
-    meio = inicio + (fim - inicio) / 2;
+  public static void processamento(int randomNumber){
+    int fim=100;
+    int inicio=1;
+    int meio =0;
+    meio = inicio + (fim - inicio) / 2;//formula padrao
+    Scanner leitura = new Scanner(System.in);
+    System.out.println("Digite o palpite : ");
+    int palpite=leitura.nextInt();
+    while(palpite===randomNumber){
+      if(fim>randomNumber){
+        fim=fim-1;
+        System.out.println(fim);
+      }
+      if(inicio<randomNumber){
+        inicio=inicio+1;
+        System.out.println(inicio);
+      }
+    } 
   }
   public static void gerador(){
     Random random = new Random();
