12769ca53fa55a31232ff67d5e797e2299b54b5f
Vinícius Bruno <vinimerces2016@gmail.com>
2025-07-01 08:25:22 -0300

    vários erros de lógica

--- lista_12_06/lista_compras.java
+++ lista_12_06/lista_compras.java
@@ -5,33 +5,32 @@ Crie um programa que permita ao usuário adicionar itens a uma lista de compras 
 import java.util.Scanner;
 import java.util.ArrayList;
 public class lista_compras{
-  public static void mostrarCompras(){
+  public static void mostrarCompras(ArrayList<String>compras){
     for(String indice: compras){
       System.out.println(indice);
     }
   }
-  public static void criar_lista(String itemAdicionado){
+  public static void verificar_lista(String itemAdicionado){
     ArrayList<String> compras = new ArrayList<String>();
     compras.add(itemAdicionado);
+    mostrarCompras(compras);
   }
   public static void adicionar(){
     Scanner leitura = new Scanner(System.in);
     System.out.println("Digite o item? ");
     String itemAdicionado= leitura.next();
-    criar_lista(itemAdicionado);
+    verificar_lista(itemAdicionado);
   }
   public static void processamento(int resposta){
     if(resposta==1){
       adicionar();
-    };
-    if(resposta==2){
-      mostrarCompras();
-    };
+    }
+
   }
   public static void main(String[] args) {
     Scanner leitura = new Scanner(System.in);
     System.out.println("Lista de Compras");
-    System.out.println("Digite:\n1º para adicionar um item;\n2º para ver o carrinho;\n3º para excluir um item;\n4º para excluir o carrinho;");
+    System.out.println("Digite:\n1º Adicionar um item;\n2ºVer o carrinho de compras;");
     int respostaUsuario= leitura.nextInt();
     processamento(respostaUsuario);
   }
