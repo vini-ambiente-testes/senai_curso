e921122161503e270d56897fc04e032a122d8fbb
Vinícius Bruno <vinimerces2016@gmail.com>
2025-07-07 12:55:34 -0300

    tentativas para deixar o código em POO

--- Caixa_Mercado/src/main/java/com/caixa_mercado/carrinho.java
+++ Caixa_Mercado/src/main/java/com/caixa_mercado/carrinho.java
@@ -1,31 +1,14 @@
-package com.caixa_mercado; // Certifique-se que o pacote aqui corresponde ao PACKAGE_BASE
-
+package com.caixa_mercado;
 
 public class Carrinho {
-    public static void Adicao() {
-        System.out.println("Hello World!");
-    }
-    public static void main(String[] args) {
-        Carrinho myObj = new Carrinho();
-        Adicao();
-    }
-}
-public class Carrinho {
-    private double largura;
-    private double altura;
+  // Atributos
+   modelo;
+  int ano;
 
-    public Carrinho(double largura, double altura) {
-        this.largura = largura;
-        this.altura = altura;
-    }
-
-    public String Adicao() {
-        return largura * altura;
-    }
-
-    public static void main(String[] args) {
-        Carrinho compras= new Carrinho();
-
-    }
+  // Método
+  void acelerar() {
+    System.out.println("O carro está acelerando!");
+  }
 }
 
+}
