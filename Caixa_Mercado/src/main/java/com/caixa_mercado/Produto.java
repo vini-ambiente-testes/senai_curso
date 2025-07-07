a1446b93d4a7e4ca98279d5c5c13bfcf87c0d4ce
vini-ambiente-testes <vini.ambiente.testes@gmail.com>
2025-07-07 23:18:28 +0000

    erros

---
+++ Caixa_Mercado/src/main/java/com/caixa_mercado/Produto.java
@@ -0,0 +1,33 @@
+package com.caixa_mercado;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Produto{
+    String nome;
+    double preco;
+
+    Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    void apresentar() {
+        System.out.println("Olá, seu produto é " + nome + " e vale " + preco + " reais");
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        ArrayList<Produto> listaProdutos = new ArrayList<>();
+        System.out.println("Me fale o nome do item?");
+        String nome=leitura.next();
+        System.out.println("Me fale o preço do item?");
+        double preco = leitura.nextDouble();
+        listaProdutos.add(new Produto(nome, preco));
+        for (Produto p : listaProdutos) {
+            p.apresentar();
+        }
+    }
+
+}
