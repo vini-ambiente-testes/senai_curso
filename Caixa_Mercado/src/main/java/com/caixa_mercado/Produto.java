75b7d66506015d8f0e932803d13424d9dc8ac308
vini-ambiente-testes <vini.ambiente.testes@gmail.com>
2025-07-07 23:39:05 +0000

    add

--- Caixa_Mercado/src/main/java/com/caixa_mercado/Produto.java
+++ Caixa_Mercado/src/main/java/com/caixa_mercado/Produto.java
@@ -2,11 +2,12 @@ package com.caixa_mercado;
 
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Produto{
+
+class Produtos{
     String nome;
     double preco;
 
-    Produto(String nome, double preco) {
+    Produtos(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
     }
@@ -16,16 +17,16 @@ public class Produto{
 
 }
 
-class Main {
+public class Produto {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-        ArrayList<Produto> listaProdutos = new ArrayList<>();
+        ArrayList<Produtos> listaProdutos = new ArrayList<>();
         System.out.println("Me fale o nome do item?");
         String nome=leitura.next();
         System.out.println("Me fale o preço do item?");
         double preco = leitura.nextDouble();
-        listaProdutos.add(new Produto(nome, preco));
-        for (Produto p : listaProdutos) {
+        listaProdutos.add(new Produtos(nome, preco));
+        for (Produtos p : listaProdutos) {
             p.apresentar();
         }
     }
